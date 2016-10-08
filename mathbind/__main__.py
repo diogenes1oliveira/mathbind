6f08165b979ea0f008ec13efb80c349a1315a727
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2016-10-08 13:09:34 -0300

    Added the command generate-math

--- mathbind/__main__.py
+++ mathbind/__main__.py
@@ -7,7 +7,7 @@ from mathbind.library import LibraryObject
 
 
 @opster.command(usage='[-d FILE] [-o FILE]')
-def main_command(output=('o','','Output file, defaults to stdout'),
+def generate_c(output=('o','','Output file, defaults to stdout'),
                  def_file=('d', '', 'JSON file with the definition of the library structure')):
     """
     A Python tool to auto-generate the boilerplate code to connect Mathematica to arbitrary external libraries.
@@ -22,11 +22,29 @@ def main_command(output=('o','','Output file, defaults to stdout'),
     fp_out.close()
     fp_in.close()
 
+@opster.command(usage='[-d FILE] [-o FILE]')
+def generate_math(libname,
+                  output=('o','','Output file, defaults to stdout'),
+                  def_file=('d', '', 'JSON file with the definition of the library structure')):
+    """
+    A Python tool to auto-generate the boilerplate code to connect Mathematica to arbitrary external libraries. This command generates the Mathematica code.
+    """
+    fp_out = open(output, 'w') if output else sys.stdout
+    fp_in = open(def_file) if def_file else sys.stdin
+
+    lib_def = json.load(fp_in)
+    lib = LibraryObject(lib_def)
+
+    fp_out.write(lib.to_mathstr(libname))
+    fp_out.close()
+    fp_in.close()
+
 
 def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-    main_command.command()
+    if argv is not None:
+        sys.argv = ['mathbind'] + argv
+
+    opster.dispatch()
 
 if __name__ == '__main__':
     main()
