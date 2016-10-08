6e8f11a152ea8b7974751737b4201b98aca15b7b
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2016-10-08 18:55:08 -0300

    Now we type manually the imports

--- mathbind/compilers/__init__.py
+++ mathbind/compilers/__init__.py
@@ -1,6 +1,2 @@
-from path import Path
-
-from mathbind.generic import iterate_folder
-
-_files = iterate_folder(__file__, '*.py', ['__init__', 'basiccompiler'])
-__all__ = [str(Path(f).basename) for f in _files if f.isfile()]
+from mathbind.compilers.compiler import Compiler
+from mathbind.compilers.gfortran import GFortranCompiler
