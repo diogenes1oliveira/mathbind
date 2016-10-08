3abb65265ead77dbf2364fb16a95c471ff1c2cda
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2016-10-08 18:45:28 -0300

    Typo in __init__.py

--- mathbind/compilers/__init__.py
+++ mathbind/compilers/__init__.py
@@ -3,4 +3,4 @@ from path import Path
 from mathbind.generic import iterate_folder
 
 _files = iterate_folder(__file__, '*.py', ['__init__', 'basiccompiler'])
-__all__ = [str(Path(f).basename) for f in _files if f.is_file()]
+__all__ = [str(Path(f).basename) for f in _files if f.isfile()]
