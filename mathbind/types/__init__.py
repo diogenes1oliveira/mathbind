d48d817b1f30c446fb2cd6f24c7fef42d3805033
Diogenes Oliveira <diogenes1oliveira+github@gmail.com>
2017-04-24 14:36:54 -0300

    Reseted completely

--- mathbind/types/__init__.py
+++ mathbind/types/__init__.py
@@ -3,9 +3,3 @@
 """
 Module with the definition of the C types.
 """
-
-from mathbind.types.basictype import BasicType
-from mathbind.types.basicvaluetype import BasicValueType
-from mathbind.types.voidtype import VoidType
-from mathbind.types.pointertype import PointerType
-from mathbind.types.arraytype import ArrayType
